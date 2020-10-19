cfa289d6a5b1ca556fd44ed67663f1c87e4834c8
Tasneem Farag <tasneem.a.farag@gmail.com>
2020-10-19 14:20:35 -0400

    making a post selectable

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,7 +8,8 @@ import './Blog.css';
 
 class Blog extends Component {
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null
     }
     componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/posts')
@@ -20,13 +21,21 @@ class Blog extends Component {
                         author: 'Tasneem'
                     }
                 })
-                this.setState({posts: updatedPosts});
+                this.setState({ posts: updatedPosts });
                 // console.log(response);
             });
     }
-    render () {
+
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id});
+    }
+
+    render() {
         const posts = this.state.posts.map((post) => {
-            return <Post key={post.id} title={post.title} author={post.author}/>
+            return <Post key={post.id}
+                title={post.title}
+                author={post.author}
+                clicked={() => this.postSelectedHandler(post.id)} />
         })
         return (
             <div>
@@ -34,7 +43,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />
